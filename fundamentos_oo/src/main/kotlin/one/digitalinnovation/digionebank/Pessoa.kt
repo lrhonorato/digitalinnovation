06711edb2d2a394b0ed98cff7524ff5c867ea97e
lrhonorato <lucas.rh@gmail.com>
2021-04-24 10:07:18 -0300

    updated to talk about abstraction

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,20 +1,6 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa{
-    val nome: String = "Jether"
-    val cpf: String = "123.123.123-12"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val jether = Pessoa()
-
-    println(jether)
-    println(jether.nome)
-    println(jether.cpf)
-    println(jether.pessoaInfo())
+    val nome: String,
+    val cpf: String
 }
